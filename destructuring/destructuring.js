8ac25340150319624091fac299a8902367900646
Juliana Sabioni <jsabioni.dev@gmail.com>
2025-05-26 18:48:13 -0300

    Create destructuring.js

---
+++ destructuring/destructuring.js
@@ -0,0 +1,11 @@
+const userNameData = ["Juliana", "Sabioni"];
+
+const firstName = userNameData[0];
+const lastName = userNameData[1];
+
+console.log(firstName + " " + lastName);
+
+//Destructuring
+const [firstName2, lastName2] = ["Juliana", "Sabioni"];
+
+console.log(firstName2 + " " + lastName2);
